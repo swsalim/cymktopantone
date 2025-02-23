b21f80c8401b79183ce002cd2e0e9e7b49cfc74f
Yuyu S <swsalim@gmail.com>
2025-02-23 22:28:52 +0800

    update year

--- components/footer.tsx
+++ components/footer.tsx
@@ -50,6 +50,8 @@ interface FooterProps extends ComponentProps<'footer'> {
 }
 
 export default function Footer({ className, ...props }: FooterProps) {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className={cn('bg-gray-900', className)} {...props}>
       <div className="mx-auto max-w-7xl px-6 pb-8 pt-16 sm:pt-24 lg:px-8 lg:pt-32">
@@ -93,8 +95,8 @@ export default function Footer({ className, ...props }: FooterProps) {
           </div>
         </div>
         <div className="mt-16 border-t border-gray-900/10 pt-8 sm:mt-20 lg:mt-24">
-          <p className="text-base/6 text-gray-300">
-            &copy; 2024{' '}
+          <p className="text-sm text-gray-300">
+            &copy; {currentYear}{' '}
             <a
               href="https://www.yuurrific.com?ref=cmyktopantone"
               className="font-medium text-violet-400 transition hover:text-violet-400"
