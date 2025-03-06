8a183281ea49eb2ea3934d3be0b0f4082fded718
Yuyu S <swsalim@gmail.com>
2025-03-07 01:38:56 +0800

    add indie boosting to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { siteConfig } from '@/config/site';
 
 import { absoluteUrl } from '@/lib/utils';
 
+import IndieBoostingAds from '@/components/ads/indie-boosting';
 import CmykPantoneContent from '@/components/cmyk-pantone-content';
 import CmykPantoneConverter from '@/components/cmyk-pantone-converter';
 import WebsiteJsonLd from '@/components/structured-data/WebsiteJsonLd';
@@ -12,6 +13,7 @@ export default function Home() {
       <WebsiteJsonLd company={siteConfig.siteName} url={absoluteUrl()} />
 
       <CmykPantoneConverter />
+      <IndieBoostingAds />
       <CmykPantoneContent />
     </>
   );
