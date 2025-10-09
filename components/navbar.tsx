198b70c283befabfdaaec3aa910898f3dcafc32b
Yuyu <swsalim@gmail.com>
2025-10-09 08:16:13 +0800

    remove advertise link

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -83,11 +83,11 @@ export const navItems: {
     name: 'More Tools',
     childItems: tools,
   },
-  {
-    name: 'Advertise',
-    childItems: [],
-    href: '/advertise',
-  },
+  // {
+  //   name: 'Advertise',
+  //   childItems: [],
+  //   href: '/advertise',
+  // },
 ];
 
 const ListItem = React.forwardRef<
