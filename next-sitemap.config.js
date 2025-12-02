fa860b4fa45259224f03a68a76e5df64f0c99491
Yuyu <swsalim@gmail.com>
2025-12-02 15:02:18 +0800

    update robots

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,10 +1,8 @@
 // Save crawling budget by not fetching SSG meta files
 const NEXT_SSG_FILES = [
-  '/*.json$',
   '/*_buildManifest.js$',
   '/*_middlewareManifest.js$',
-  '/*_ssgManifest.js$',
-  '/*.js$',
+  '/*_ssgManifest.js$'
 ];
 
 const exclude = ['/dashboard*', '/404', '/api*', '/login', '/server-sitemap.xml'];
